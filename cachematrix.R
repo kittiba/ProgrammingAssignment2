3e4a79a134c5735dfd7d6838e117c55675894a0d
kittiba <doddamreddy@gmail.com>
2014-05-20 11:33:12 -0400

    First commit for this assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program - cachematrix.R will have the functions necessary to save time 
+## in calculating the inverse of matrix if previously calculated. 
+## This assumes however that the contents of the matrix did not change since
+## it persisted the initial matrix inverse calculation.
 
-## Write a short comment describing this function
+## This function makeCacheMatrix returns a vector of functions to
+## set, get, setinverse and getinverse of a given matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        print("Hello Kittiba")
 }
 
 
-## Write a short comment describing this function
+## This function cacheSolve will return the cached value of the matrix inverse if
+## previously calculated or returns the inverse of the matrix by calculating
+## for the first time.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
