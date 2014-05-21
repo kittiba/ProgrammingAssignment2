34f3e89ce476a95985d1ce62fad680ef2b37ae2d
kittiba <doddamreddy@gmail.com>
2014-05-21 08:18:00 -0400

    Tsted and working

--- cachematrix.R
+++ cachematrix.R
@@ -3,18 +3,44 @@
 ## This assumes however that the contents of the matrix did not change since
 ## it persisted the initial matrix inverse calculation.
 
-## This function makeCacheMatrix returns a vector of functions to
-## set, get, setinverse and getinverse of a given matrix
+## This function makeCacheMatrix returns a vector which is really has a function
+## -- to set the value of the Vector
+## -- get the value of the vector
+## -- setinverse of a matrix
+## -- getinverse of a given matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-        print("Hello Kittiba")
+        z<-matrix()
+        
+        set<-function(y){
+                x<<-y
+                z<<-NULL
+        }
+                
+        get <- function() x
+        setinverse <- function(inverse) z <<- inverse
+        getinverse <- function() z
+        
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## This function cacheSolve will return the cached value of the matrix inverse if
-## previously calculated or returns the inverse of the matrix by calculating
-## for the first time.
+## This function cacheSolve will return the cached value of the inverse of matrix
+## if previously calculated or returns the inverse by calculating
+## for the first time. This takes the list that we created in makeCacheMatrix
+## as its input
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        z<- x$getinverse()
+        
+        if(!is.na(z[1,1])) {
+                message("getting cached data")
+                return(z)
+        }
+        data <- x$get()
+        z <- solve(data, ...)
+        x$setinverse(z)
+        z
 }
